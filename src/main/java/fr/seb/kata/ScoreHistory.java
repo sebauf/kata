4f5bd602c53c505ba25ba9666c1cfe4160c7d8e5
seb <sebastien.aufroy@gmail.com>
2024-10-22 21:21:29 +0200

    TU and refractoring

---
+++ src/main/java/fr/seb/kata/ScoreHistory.java
@@ -0,0 +1,5 @@
+package fr.seb.kata;
+
+public record ScoreHistory(Scores playerAScore, Scores playerBScore) {
+
+}
--- src/test/java/fr/seb/kata/TennisGameTest.java
+++ src/test/java/fr/seb/kata/TennisGameTest.java
@@ -1,13 +1,80 @@
 package fr.seb.kata;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class TennisGameTest {
 
-	@Test
-	void classicGameTest() {
-		assertDoesNotThrow(() -> new TennisGame().play("AAAA"));
-	}
+  @Test
+  void testClassicGame() {
+    ScoreHistory scoreRound1 = new ScoreHistory(Scores.FIFTEEN, Scores.ZERO);
+    ScoreHistory scoreRound2 = new ScoreHistory(Scores.THIRTY, Scores.ZERO);
+    ScoreHistory scoreRound3 = new ScoreHistory(Scores.FORTY, Scores.ZERO);
+    var expectedMatchResult = new MatchResult(List.of(scoreRound1, scoreRound2, scoreRound3),
+        Player.builder().name("A").score(Scores.FORTY).build());
+    var matchResult = new TennisGame().play("AAAA");
+
+    assertEquals(matchResult, expectedMatchResult);
+  }
+
+  @Test
+  void testAdvantageForA() {
+
+    ScoreHistory scoreRound1 = new ScoreHistory(Scores.FIFTEEN, Scores.ZERO);
+    ScoreHistory scoreRound2 = new ScoreHistory(Scores.THIRTY, Scores.ZERO);
+    ScoreHistory scoreRound3 = new ScoreHistory(Scores.FORTY, Scores.ZERO);
+    ScoreHistory scoreRound4 = new ScoreHistory(Scores.FORTY, Scores.FIFTEEN);
+    ScoreHistory scoreRound5 = new ScoreHistory(Scores.FORTY, Scores.THIRTY);
+    ScoreHistory scoreRound6 = new ScoreHistory(Scores.FORTY, Scores.FORTY);
+    ScoreHistory scoreRound7 = new ScoreHistory(Scores.ADVANTAGE, Scores.FORTY);
+    ScoreHistory scoreRound8 = new ScoreHistory(Scores.FORTY, Scores.FORTY);
+    ScoreHistory scoreRound9 = new ScoreHistory(Scores.ADVANTAGE, Scores.FORTY);
+    var expectedMatchResult = new MatchResult(
+        List.of(scoreRound1, scoreRound2, scoreRound3, scoreRound4, scoreRound5, scoreRound6,
+            scoreRound7, scoreRound8, scoreRound9),
+        Player.builder().name("A").score(Scores.ADVANTAGE).build());
+    var matchResult = new TennisGame().play("AAABBBABAA");
+
+    assertEquals(matchResult, expectedMatchResult);
+  }
+
+  @Test
+  void testNoWinner() {
+    ScoreHistory scoreRound1 = new ScoreHistory(Scores.FIFTEEN, Scores.ZERO);
+    ScoreHistory scoreRound2 = new ScoreHistory(Scores.THIRTY, Scores.ZERO);
+    ScoreHistory scoreRound3 = new ScoreHistory(Scores.FORTY, Scores.ZERO);
+    ScoreHistory scoreRound4 = new ScoreHistory(Scores.FORTY, Scores.FIFTEEN);
+    ScoreHistory scoreRound5 = new ScoreHistory(Scores.FORTY, Scores.THIRTY);
+    ScoreHistory scoreRound6 = new ScoreHistory(Scores.FORTY, Scores.FORTY);
+    ScoreHistory scoreRound7 = new ScoreHistory(Scores.ADVANTAGE, Scores.FORTY);
+    ScoreHistory scoreRound8 = new ScoreHistory(Scores.FORTY, Scores.FORTY);
+    var expectedMatchResult = new MatchResult(List.of(scoreRound1, scoreRound2, scoreRound3,
+        scoreRound4, scoreRound5, scoreRound6, scoreRound7, scoreRound8), null);
+
+    var matchResult = new TennisGame().play("AAABBBAB");
+    assertEquals(matchResult, expectedMatchResult);
+  }
+
+  @Test
+  void testNoInput() {
+    var matchResult = new TennisGame().play("");
+    assertNull(matchResult);
+  }
+
+  @Test
+  void testNullInput() {
+    var matchResult = new TennisGame().play(null);
+    assertNull(matchResult);
+  }
+
+  @Test
+  void testBadInput() {
+    ScoreHistory scoreRound1 = new ScoreHistory(Scores.ZERO, Scores.ZERO);
+    var expectedMatchResult = new MatchResult(List.of(scoreRound1), null);
+    var matchResult = new TennisGame().play("X");
+    assertEquals(matchResult, expectedMatchResult);
+  }
+
 }
